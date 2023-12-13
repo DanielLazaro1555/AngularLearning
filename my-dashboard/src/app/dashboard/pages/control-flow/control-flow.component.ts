8f1d89bb073bc09c8ec136a84736dd1aef435c00
danielhuaman <danielsam_m@hotmail.com>
2023-12-12 22:07:41 -0500

    Nuevo cambio

--- my-dashboard/src/app/dashboard/pages/control-flow/control-flow.component.ts
+++ my-dashboard/src/app/dashboard/pages/control-flow/control-flow.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+type Grade = 'A'|'B'|'F';
 
 @Component({
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './control-flow.component.html',
   styles: ``
 })
 export default class ControlFlowComponent {
 
+  public showContent = signal(false);
+  public grade = signal('A');
+
+  public frameworks = signal(['Angular','Vue','Svelte','Qwik','React']);
+  public frameworks2 = signal([]);
+  public toggleContent(){
+    this.showContent.update(value => !value);
+  }
 }
